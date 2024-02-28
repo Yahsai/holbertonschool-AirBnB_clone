53abc226fd479bcb79c973ebfd2d4171e2d5fac0
Yahsai <yahsaisantana1@gmail.com>
2024-02-27 23:30:47 -0400

    Tests models

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import unittest
+from models.amenity import Amenity
+
+class TestAmenity(unittest.TestCase):
+
+    def test_default_values(self):
+        amenity = Amenity()
+        self.assertEqual(amenity.name, "")
+
+if __name__ == '__main__':
+    unittest.main()
