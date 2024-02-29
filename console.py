6cc397bfb379ae99107a56b56a4c41189113e35b
Yahsai <yahsaisantana1@gmail.com>
2024-02-29 10:24:30 -0400

    Update console file

--- console.py
+++ console.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Console module for the AirBnB project.
 """
 
 import cmd
-import sys
-from models.base_model import BaseModel
 from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -27,18 +26,33 @@ class HBNBCommand(cmd.Cmd):
         print("")  # New line for better formatting
         return True
 
+    def check_class(self, class_name):
+        """Check if class exists."""
+        try:
+            cls = eval(class_name)
+            return True
+        except NameError:
+            return False
+
+    def check_id(self, class_name, instance_id):
+        """Check if instance id exists."""
+        key = "{}.{}".format(class_name, instance_id)
+        objects = storage.all()
+        return key in objects
+
     def do_create(self, arg):
         """Creates a new instance of BaseModel."""
         if not arg:
             print("** class name missing **")
             return
 
-        try:
-            new_instance = eval(arg)()
-            new_instance.save()
-            print(new_instance.id)
-        except NameError:
+        if not self.check_class(arg):
             print("** class doesn't exist **")
+            return
+
+        new_instance = eval(arg)()
+        new_instance.save()
+        print(new_instance.id)
 
     def do_show(self, arg):
         """Prints the string representation of an instance."""
@@ -46,9 +60,8 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        try:
-            cls = eval(args[0])
-        except NameError:
+
+        if not self.check_class(args[0]):
             print("** class doesn't exist **")
             return
 
@@ -56,12 +69,12 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
             return
 
-        key = "{}.{}".format(args[0], args[1])
-        objects = storage.all()
-        if key in objects:
-            print(objects[key])
-        else:
+        if not self.check_id(args[0], args[1]):
             print("** no instance found **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        print(storage.all()[key])
 
     def do_destroy(self, arg):
         """Deletes an instance based on the class name and id."""
@@ -69,9 +82,8 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        try:
-            cls = eval(args[0])
-        except NameError:
+
+        if not self.check_class(args[0]):
             print("** class doesn't exist **")
             return
 
@@ -79,27 +91,26 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
             return
 
-        key = "{}.{}".format(args[0], args[1])
-        objects = storage.all()
-        if key in objects:
-            del objects[key]
-            storage.save()
-        else:
+        if not self.check_id(args[0], args[1]):
             print("** no instance found **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        del storage.all()[key]
+        storage.save()
 
     def do_all(self, arg):
         """Prints all string representation of all instances."""
         args = arg.split()
         objects = storage.all()
         result = []
+
         if not arg:
             for key, value in objects.items():
                 result.append(str(value))
             print(result)
         else:
-            try:
-                cls = eval(args[0])
-            except NameError:
+            if not self.check_class(args[0]):
                 print("** class doesn't exist **")
                 return
 
@@ -114,9 +125,8 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        try:
-            cls = eval(args[0])
-        except NameError:
+
+        if not self.check_class(args[0]):
             print("** class doesn't exist **")
             return
 
@@ -124,9 +134,7 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
             return
 
-        key = "{}.{}".format(args[0], args[1])
-        objects = storage.all()
-        if key not in objects:
+        if not self.check_id(args[0], args[1]):
             print("** no instance found **")
             return
 
@@ -138,8 +146,10 @@ class HBNBCommand(cmd.Cmd):
             print("** value missing **")
             return
 
-        setattr(objects[key], args[2], eval(args[3]))
-        objects[key].save()
+        key = "{}.{}".format(args[0], args[1])
+        obj = storage.all()[key]
+        setattr(obj, args[2], eval(args[3]))
+        obj.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
