124b05eddfdfb22bbc6c80a1ee416b9d917506bc
Yahsai <yahsaisantana1@gmail.com>
2024-02-29 10:18:42 -0400

    Update console file task 7

--- console.py
+++ console.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-#!/usr/bin/env python3
 """
 Console module for the AirBnB project.
 """
 
 import cmd
+import sys
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """
     HBNBCommand class for the command interpreter.
     """
     prompt = "(hbnb) "
+
+    def emptyline(self):
+        """Do nothing on empty line."""
+        pass
 
     def do_quit(self, arg):
         """Quit command to exit the program."""
@@ -20,6 +26,120 @@ class HBNBCommand(cmd.Cmd):
         """Exit the program with EOF (Ctrl-D)."""
         print("")  # New line for better formatting
         return True
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel."""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        try:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        try:
+            cls = eval(args[0])
+        except NameError:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        objects = storage.all()
+        if key in objects:
+            print(objects[key])
+        else:
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        try:
+            cls = eval(args[0])
+        except NameError:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        objects = storage.all()
+        if key in objects:
+            del objects[key]
+            storage.save()
+        else:
+            print("** no instance found **")
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances."""
+        args = arg.split()
+        objects = storage.all()
+        result = []
+        if not arg:
+            for key, value in objects.items():
+                result.append(str(value))
+            print(result)
+        else:
+            try:
+                cls = eval(args[0])
+            except NameError:
+                print("** class doesn't exist **")
+                return
+
+            for key, value in objects.items():
+                if key.split(".")[0] == args[0]:
+                    result.append(str(value))
+            print(result)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        try:
+            cls = eval(args[0])
+        except NameError:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        objects = storage.all()
+        if key not in objects:
+            print("** no instance found **")
+            return
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        setattr(objects[key], args[2], eval(args[3]))
+        objects[key].save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
