04600a3ca3e7eeccf28c9fc6f52b38c000ec3be1
Yahsai <yahsaisantana1@gmail.com>
2024-02-29 11:37:08 -0400

    Update state

--- models/state.py
+++ models/state.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""State class"""
 from models.base_model import BaseModel
 
+
 class State(BaseModel):
+    """attributes of the class State"""
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = ""
