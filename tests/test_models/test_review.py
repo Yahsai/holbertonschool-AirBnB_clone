f57c8d12080a0e78e792a9ba469684858a228aec
Yahsai <yahsaisantana1@gmail.com>
2024-02-29 11:45:21 -0400

    Test review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -3,12 +3,31 @@ import unittest
 from models.review import Review
 
 class TestReview(unittest.TestCase):
+    """Test case for the Review class."""
+    def setUp(self):
+        """Set up for the tests."""
+        self.review1 = Review()
+        self.review2 = Review(place_id="place_id_1", user_id="user_id_1",
+                              text="Great place!")
 
-    def test_default_values(self):
-        review = Review()
-        self.assertEqual(review.place_id, "")
-        self.assertEqual(review.user_id, "")
-        self.assertEqual(review.text, "")
+    def test_init_without_args(self):
+        """Test initialization without arguments."""
+        self.assertIsInstance(self.review1, Review)
+        self.assertIs(self.review1.place_id, "")
+        self.assertIs(self.review1.user_id, "")
+        self.assertIs(self.review1.text, "")
+
+    def test_init_with_args(self):
+        """Test initialization with arguments."""
+        self.assertEqual(self.review2.place_id, "place_id_1")
+        self.assertEqual(self.review2.user_id, "user_id_1")
+        self.assertEqual(self.review2.text, "Great place!")
+
+    def test_attributes_types(self):
+        """Test the types of attributes."""
+        self.assertIsInstance(self.review1.place_id, str)
+        self.assertIsInstance(self.review1.user_id, str)
+        self.assertIsInstance(self.review1.text, str)
 
 if __name__ == '__main__':
     unittest.main()
